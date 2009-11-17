71634440d69249d8dc08606eff7b4d893e6034a6
Satoshi Nishimura <nishim314@gmail.com>
2009-11-17 18:19:18 +0900

    Adding Component.php file.

---
+++ components/Laiz/Component.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Laiz Component Interface to Auto Register Interface
+ *
+ * PHP versions 5
+ *
+ * @package   Laiz
+ * @author    Satoshi Nishimura <nishim314@gmail.com>
+ * @copyright 2005-2009 Satoshi Nishimura
+ * @license   http://opensource.org/licenses/bsd-license.php The BSD License
+ */
+
+/**
+ * Laiz Component Interface to Auto Register Interface
+ *
+ * @package   Laiz
+ * @author    Satoshi Nishimura <nishim314@gmail.com>
+ */
+interface Laiz_Component{}
+
