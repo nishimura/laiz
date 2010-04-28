e38a445572b8b4bcfa3a721b23bb1e0d5b21738e
Satoshi Nishimura <nishim314@gmail.com>
2010-04-28 16:06:34 +0900

    Adding comment in lib/db/Factory.php.

--- laiz/lib/db/Factory.php
+++ laiz/lib/db/Factory.php
@@ -4,5 +4,12 @@ namespace laiz\lib\db;
 
 interface Factory
 {
+    /**
+     * Returns Database Utility Object.
+     *
+     * @param string $name
+     * @return mixed Iterator or Orm or Driver.
+     * @see Factory_Selector
+     */
     public function create($name);
 }
