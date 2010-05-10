275512bd33a7150608f2040786e86cb86392f2a9
Satoshi Nishimura <nishim314@gmail.com>
2010-05-10 20:02:58 +0900

    Adding util/Url.php.

---
+++ laiz/lib/util/Url.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Url Utility Class.
+ *
+ * PHP versions 5.3
+ *
+ * @package   Laiz
+ * @author    Satoshi Nishimura <nishim314@gmail.com>
+ * @copyright 2010 Satoshi Nishimura
+ */
+
+namespace laiz\lib\util;
+
+/**
+ * Url Utility Class.
+ *
+ * @package   Laiz
+ * @author    Satoshi Nishimura <nishim314@gmail.com>
+ */
+class Url
+{
+    private $https;
+    private $host;
+
+    public function __construct()
+    {
+        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+            $this->https = true;
+        else
+            $this->https = false;
+        $this->host = $_SERVER['HTTP_HOST'];
+    }
+
+    public function setHttps($flag)
+    {
+        $this->https = $flag;
+    }
+
+    public function switchHttps()
+    {
+        $this->https = !$this->https;
+    }
+
+    public function setHost($host)
+    {
+        $this->host = $host;
+    }
+
+    public function getRoot()
+    {
+        $ret = '';
+        if ($this->https)
+            $ret .= 'https://';
+        else
+            $ret .= 'http://';
+        $ret .= $this->host . '/';
+    }
+}
