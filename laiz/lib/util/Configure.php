5a1081ea773d83256e564bb3006a09d756ef6db2
Satoshi Nishimura <nishim314@gmail.com>
2010-05-13 17:59:27 +0900

    Adding Configure class for user.

---
+++ laiz/lib/util/Configure.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Configure Class for User.
+ *
+ * PHP versions 5.3
+ *
+ * @package   Laiz
+ * @author    Satoshi Nishimura <nishim314@gmail.com>
+ * @copyright 2010 Satoshi Nishimura
+ */
+
+namespace laiz\lib\util\Configure;
+
+use laiz\core\Configure as Core;
+
+/**
+ * Configure Class for User.
+ *
+ * @package   Laiz
+ * @author    Satoshi Nishimura <nishim314@gmail.com>
+ */
+class Configure
+{
+    static public function getCacheDir()
+    {
+        $base = Core::get('base');
+        return $base['CACHE_DIR'];
+    }
+}
