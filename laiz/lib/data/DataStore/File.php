308526d0daeceecb7c7c5f55f239b460286dec7a
Satoshi Nishimura <nishim314@gmail.com>
2010-05-14 12:25:45 +0900

    Adding method in DataStore_File.

--- laiz/lib/data/DataStore/File.php
+++ laiz/lib/data/DataStore/File.php
@@ -11,6 +11,8 @@
 
 namespace laiz\lib\data;
 
+use \laiz\lib\util\Configure;
+
 /**
  * Data Store Using Files.
  *
@@ -21,35 +23,54 @@ namespace laiz\lib\data;
  * @package   Laiz
  * @author    Satoshi Nishimura <nishim314@gmail.com>
  */
-class DataStore_File implements DataStore
+class DataStore_File1 implements DataStore
 {
-    // ==TODO==
-    public function __construct()
-    {
-        
-    }
+    private $scope = 'laiz_datastore_file';
 
     public function setDsn(Array $dsn)
     {
-
+        if (!isset($dsn['scope']))
+            return false;
+        $this->scope = $dsn['scope'];
+        return true;
     }
 
     public function set($key, $value, $expire = null)
     {
-
+        $file = $this->getFilePath($key);
+        return file_put_contents($file, serialize($value));
     }
 
     public function delete($key)
     {
+        $file = $this->getFilePath($key);
+        return unlink($file);
     }
 
     public function get($key)
     {
+        $file = $this->getFilePath($key);
+        if (!file_exists($file))
+            return null;
 
+        return unserialize(file_get_contents($file));
     }
 
     public function clear()
     {
+        // TODO
+    }
 
+    private function getFilePath($key)
+    {
+        $cacheDir = Configure::getCacheDir()
+            . 'laiz_datastore_file/'
+            . $this->scope;
+        if (!is_dir($cacheDir))
+            mkdir($cacheDir, 0777, true); // mkdir -p
+
+        $file = $cacheDir . '/'
+            . $key . '.serialized';
+        return $file;
     }
 }
