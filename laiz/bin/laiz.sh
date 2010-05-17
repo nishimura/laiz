2e169b7696f01655ce1f8748c7325a21fecaa5ce
Satoshi Nishimura <nishim314@gmail.com>
2010-05-17 17:18:02 +0900

    Changing comment in laiz.sh.

--- laiz/bin/laiz.sh
+++ laiz/bin/laiz.sh
@@ -3,7 +3,8 @@
 if [ "$BASE" == "" -a "$PROJECT_BASE_DIR" == "" ]; then
     echo "Need configure"
     echo "  export PROJECT_BASE_DIR=<project base dir>"
-    echo "  source bin/setpath.sh"
+    BINDIR=$(cd $(dirname $0) && pwd)
+    echo "  export PATH=$BINDIR:\$PATH"
     exit 1
 fi
 
